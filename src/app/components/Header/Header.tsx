d2980426a41f6c884514711f6ae17d77d38946dc
Jayson Mourier <jayson.mourier@proton.me>
2024-12-12 18:03:56 +0100

    test

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,53 +1,32 @@
-"use client";
-
-import { useEffect, useState } from 'react';
 import styles from '@/app/components/Header/Header.module.css';
-import BlurAnimation from '../BlurAnimation/BlurAnimation';
-import animationData from '@/app/assets/lotties/object1.json';
+import Image from 'next/image';
 
 export default function Header() {
-    const [offsetY, setOffsetY] = useState(0);
-
-    const handleScroll = () => {
-        setOffsetY(window.scrollY);
-    }
-
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll);
-        return () => window.removeEventListener('scroll', handleScroll);
-    }, []);
-
     return (
-        <header className={`${styles.header} flex flex-column justify-center`}>
-            <BlurAnimation animation={animationData} style={
-                {
-                    top: "0px",
-                    left: "0px",
-                    position: "absolute",
-                    transform: `translateY(${offsetY * 0.8}px)`
-                }
-            } />
-            <div className={`${styles.infos} flex flex-column justify-around`}>
-                <div className={styles.name}>
-                    <p className={styles.firstname}>jayson</p>
+        <header className={`${styles.header} flex flex-column justify-around`}>
+            <div className={styles.infos}>
+                <div className={styles.names}>
+                    <p>jayson</p>
                     <p className={styles.lastname}>mourier</p>
                 </div>
-
-                <div className={`${styles.job} self-end`}>
-                    <p>Développeur</p>
-                </div>
-
-                <div className={styles.type}>
-                    <p>Mobile</p>
-                    <p>& Web</p>
-                </div>
+                <p className={`${styles.dev} self-end`}>développeur</p>
+                <p className={styles.devtype}>mobile<br />& web</p>
             </div>
-            <div
-                className={styles.imageParallax}
-                style={{
-                    transform: `translateY(${offsetY * 0.5}px)`
-                }}
-            ></div>
+            <div className={styles.image}>
+                <Image
+                    src={'/images/person.webp'}
+                    alt="A simple person"
+                    priority
+                    fill
+                    style={
+                        {
+                            objectFit: "contain",
+                            objectPosition: "center",
+                            zIndex: "0",
+                        }
+                    }
+                />
+            </div>
         </header>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import Header from '@/app/components/Header/Header';
-import BigBlock from './components/Header/BigBlock/BigBlock';
 import styles from '@/app/page.module.css';
 
 export default function Home() {
   return (
     <div className={styles.app}>
       <Header />
-      <BigBlock />
     </div>
   );
 }
